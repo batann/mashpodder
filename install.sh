9271d43694c13c0bc273ce27ea48b023e7e65999
batann <122584289+batann@users.noreply.github.com>
2024-09-12 22:39:57 +1000

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+clear
+echo -e "\033[34mFiles are configured to work from a Base-Directory on \033[31m/media/batan/mashpodder/\033[0m"
+echo -e "Nothing to install, but LC-YAD-PODCAST is still to be made functional"
+
